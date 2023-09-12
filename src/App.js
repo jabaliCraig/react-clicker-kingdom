cfca25cfb4f333956e62c8053141de4e6ad0be5b
Craig <71907929+jabaliCraig@users.noreply.github.com>
2023-09-12 07:40:02 -0600

    basic component structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Main from './components/Main';
+import Success from './components/Success';
+
 
 function App() {
-  return (
-    <div className="App">
-      Hello?
-    </div>
-  );
+
+	return (
+		<Router>
+			<div className='app-container'>
+				<Routes>
+					<Route 
+					  path='/' 
+						element={<Main />} 
+					/>
+					<Route 
+					  path='/success' 
+						element={<Success />} 
+					/>
+				</Routes>
+			</div>
+		</Router>
+	)
 }
 
 export default App;
---
+++ src/components/Success.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Success = () => {
+	return (
+		<div>
+			This is the SUCCESS component.
+		</div>
+	)
+}
+
+export default Success
+
